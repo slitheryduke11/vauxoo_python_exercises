010b181a5f2f7e6499f970b8079a5de65eb36ad6
slitheryduke11 <hedguhar@vauxoo.com>
2023-01-18 11:22:17 -0600

    Added comments to Chapter 1 scripts.

--- chapter_1/A5.py
+++ chapter_1/A5.py
@@ -1,4 +1,5 @@
-# Ejercicio 5
+#!/usr/bin/env python3
+"""Program that prints a greeting."""
 
 if __name__ == '__main__':
 	print ('¡Hola, mundo!')
--- chapter_1/A7.py
+++ chapter_1/A7.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+"""Program that increments a variable by one and prints the result."""
+
 if __name__ == '__main__':
 	x = 43
 	x = x + 1
